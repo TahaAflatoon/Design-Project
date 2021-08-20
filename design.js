3bd4b5c7858df64b52bd608b19ca35a12547b241
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2021-08-20 20:02:16 +0430

    completed all files edition

--- design.js
+++ design.js
@@ -1,9 +1,11 @@
 /*JQuery*/
 $.noConflict()
 jQuery(document).ready(function($) {
+    //Close button
     $('.closebtn').click(function() {
         $('.header').slideUp(500)
     }),
+    //Open button
     $('.openbtn').click(function() {
         $('.header').slideDown(500)
     })
