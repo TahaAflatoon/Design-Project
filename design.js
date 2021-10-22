0186c959c29a9caec426a98411a6cef7404fe024
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2021-10-22 20:10:08 +0330

    improved syntax & readability

--- design.js
+++ design.js
@@ -1,14 +1,14 @@
 /*JQuery*/
 $.noConflict()
 jQuery(document).ready(function($) {
-    //Close button
+    // Close button
     $('.closebtn').click(function() {
         $('.header').slideUp(500)
     }),
-    //Open button
+    // Open button
     $('.openbtn').click(function() {
         $('.header').slideDown(500)
     })
 });
 
-/*JavaScript*/
+// JavaScript
